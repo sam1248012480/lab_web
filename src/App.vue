cad8c6d9cff0a3f8fb9d910360d8409ad03c0dc3
JusesCheng <c6570002@gmail.com>
2020-04-06 18:50:18 +0800

    新增部分功能

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <v-app class="grey lighten-4">
     <NavBar></NavBar>
+    <!-- 裡面一定要用到不然會報錯(step.3) -->
+    <Carousels></Carousels>
+    <Bottom_area></Bottom_area>
+    
+
+
     <v-content>
      <router-view></router-view>
     </v-content>
@@ -10,16 +16,23 @@
 
 <script>
 import NavBar from '@/components/Navbar'
+//有新的Components這邊要先import(step.1)
+import Carousels from '@/components/Carousels'
+import Bottom_area from '@/components/Bottom_area'
 
 export default {
   name: 'App',
 
-  components: {
-    NavBar
+  components: { 
+    NavBar,
+    //此處要註冊(step.2)
+    Carousels,
+    Bottom_area
+    
   },
 
   data: () => ({
-    //
+    
   }),
 };
 </script>
---
+++ src/components/Bottom_area.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-row align="center">  
+      
+    <v-col class="text-center" cols="12" sm="4">
+      <div class="my-2">
+        <v-btn large color="#039BE5" class="white--text">台北</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn large color="#039BE5" class="white--text">嘉義</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn large color="#039BE5" class="white--text">台中</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn large color= "#039BE5" class="white--text">雲林</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn large color= "#039BE5" class="white--text">苗栗</v-btn>
+      </div>
+    </v-col>
+
+    <v-col class="text-center" cols="12" sm="4">
+      <div class="my-2">
+        <v-btn large color="#039BE5" class="white--text">基隆</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn large color="#039BE5" class="white--text">高雄</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn large color="#039BE5" class="white--text">屏東</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn large color= "#039BE5" class="white--text">花蓮</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn large color= "#039BE5" class="white--text">彰化</v-btn>
+      </div>
+    </v-col>
+
+    <v-col class="text-center" cols="12" sm="4">
+      <div class="my-2">
+        <v-btn large color="#039BE5" class="white--text">澎湖</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn large color="#039BE5" class="white--text">台東</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn large color="#039BE5" class="white--text">南投</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn large color= "#039BE5" class="white--text">宜蘭</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn large color= "#039BE5" class="white--text">新竹</v-btn>
+      </div>
+    </v-col>
+
+
+
+
+
+  </v-row>
+</template>
