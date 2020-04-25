7d968b7e92aaf7308ddeee9ccdb0f32ac397ef5f
sam1248012480 <sam1248012480@gmail.com>
2020-04-25 19:10:25 +0800

    修改圖片擺放位置與功能整合

--- src/views/Infopage.vue
+++ src/views/Infopage.vue
@@ -15,7 +15,7 @@
             <template v-slot:default>
               <thead></thead>
               <tbody>
-                <tr v-for="item in desserts" :key="item.aa">
+                <tr v-for="item in desserts" :key="item.name">
                   <td>{{ item.name }}</td>
                   <td>{{ item.calories }}</td>
                 </tr>
@@ -26,38 +26,33 @@
       </v-col>
     </v-row>
 
-    <v-row justify="space-around">
-      <v-col cols="3" md="3">
-        <v-img
-          src="https://instagram.frmq2-2.fna.fbcdn.net/v/t51.2885-15/e35/33102682_133926010807761_8907972298883989504_n.jpg?_nc_ht=instagram.frmq2-2.fna.fbcdn.net&_nc_cat=109&_nc_ohc=GYd1hsfNce8AX8TD1JR&oh=0dcb71ab922a591f8fbaa967e3ef48b8&oe=5EBA6A0B"
-          aspect-ratio="1"
-        ></v-img>
-      </v-col>
-      <v-col cols="3" md="3">
-        <v-img
-          src="https://instagram.frmq2-1.fna.fbcdn.net/v/t51.2885-15/e35/32359053_1748181835270702_4491973812325187584_n.jpg?_nc_ht=instagram.frmq2-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=UlLUN5zLcAwAX_T9dwC&oh=941fa231553df88e5484a9af8d963fb1&oe=5EBAD76B"
-          aspect-ratio="1"
-        ></v-img>
-      </v-col>
-      <v-col cols="3" md="3">
-        <v-img
-          src="https://instagram.frmq2-2.fna.fbcdn.net/v/t51.2885-15/e35/32233075_567321030290575_3045565007560441856_n.jpg?_nc_ht=instagram.frmq2-2.fna.fbcdn.net&_nc_cat=107&_nc_ohc=CAoYVNrOqVgAX_jdMzY&oh=6bcef31a4fb50ec7caafc290459917bc&oe=5EBC6027"
-          aspect-ratio="1"
-        ></v-img>
-      </v-col>
-      <v-col cols="3" md="3">
-        <v-img
-          src="https://instagram.frmq2-2.fna.fbcdn.net/v/t51.2885-15/e35/31290461_212833709322687_2491188745164816384_n.jpg?_nc_ht=instagram.frmq2-2.fna.fbcdn.net&_nc_cat=103&_nc_ohc=_cznVTUsRGMAX9JlZn5&oh=4311d43a430e8ef7f2faa49a4321664f&oe=5EBD4DB0"
-          aspect-ratio="1"
-        ></v-img>
-      </v-col>
-    </v-row>
+    <viewer>
+      <v-row>
+        <v-col cols="12">
+          <v-card>
+            <v-container fluid>
+              <v-row>
+                <v-col v-for="src in imgs" :key="src.url" class="d-flex child-flex" cols="4">
+                  <v-card flat tile class="d-flex">
+                    <img :src="src.url" style="width:100%; height:100%;" />
+                    <template v-slot:placeholder>
+                      <v-row class="fill-height ma-0" align="center" justify="center">
+                        <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                      </v-row>
+                    </template>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card>
+        </v-col>
+      </v-row>
+    </viewer>
 
     <v-row justify="space-around">
-        <v-col   >
+      <v-col>
         <video-player
           class="video-player vjs-custom-skin"
-          
           ref="videoPlayer"
           :playsinline="true"
           :options="{
@@ -67,25 +62,20 @@
       }],
           
     }"
-  ></video-player>
+        ></video-player>
       </v-col>
     </v-row>
-
   </v-container>
 </template>
 
    
-
-
-
 <script>
-import { videoPlayer } from 'vue-video-player'
-import 'video.js/dist/video-js.css'
-import 'vue-video-player/src/custom-theme.css'
-
+import { videoPlayer } from "vue-video-player";
+import "video.js/dist/video-js.css";
+import "vue-video-player/src/custom-theme.css";
 
 export default {
-    components: {
+  components: {
     videoPlayer
   },
 
@@ -96,48 +86,46 @@ export default {
     aa: 2,
     desserts: [
       {
-        name: "國別",
-        calories: "台?"
+        name: "名稱",
+        calories: "米米"
       },
       {
-        name: "年齡",
-        calories: "35"
+        name: "年紀",
+        calories: "0"
       },
       {
-        name: "體重",
-        calories: "75"
+        name: "條件(身高/胸圍)",
+        calories: "0"
       },
       {
-        name: "環境",
-        calories: "獨棟大樓"
+        name: "價格(固定)",
+        calories: "10000000"
       },
       {
-        name: "時間",
-        calories: "100"
+        name: "服務地區",
+        calories: "台灣"
       },
       {
-        name: "定點長鐘",
-        calories: "3500"
+        name: "服務內容",
+        calories: "0"
       },
       {
-        name: "編號",
-        calories: "111"
+        name: "定點或外送",
+        calories: "外送"
+      }
+    ],
+    imgs: [
+      {
+        url:
+          "https://instagram.frmq2-2.fna.fbcdn.net/v/t51.2885-15/e35/33102682_133926010807761_8907972298883989504_n.jpg?_nc_ht=instagram.frmq2-2.fna.fbcdn.net&_nc_cat=109&_nc_ohc=GYd1hsfNce8AX8TD1JR&oh=0dcb71ab922a591f8fbaa967e3ef48b8&oe=5EBA6A0B"
       },
       {
-        name: "身高",
-        calories: "162"
+        url:
+          "https://instagram.frmq2-1.fna.fbcdn.net/v/t51.2885-15/e35/32359053_1748181835270702_4491973812325187584_n.jpg?_nc_ht=instagram.frmq2-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=UlLUN5zLcAwAX_T9dwC&oh=941fa231553df88e5484a9af8d963fb1&oe=5EBAD76B"
       },
       {
-        name: "三圍",
-        calories: "???"
-      },
-      {
-        name: "類型",
-        calories: "???"
-      },
-      {
-        name: "時間",
-        calories: "???"
+        url:
+          "https://instagram.frmq2-2.fna.fbcdn.net/v/t51.2885-15/e35/32233075_567321030290575_3045565007560441856_n.jpg?_nc_ht=instagram.frmq2-2.fna.fbcdn.net&_nc_cat=107&_nc_ohc=CAoYVNrOqVgAX_jdMzY&oh=6bcef31a4fb50ec7caafc290459917bc&oe=5EBC6027"
       }
     ]
   })
