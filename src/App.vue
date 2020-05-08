40dcb594bf835865da4f7e2a5daaca5f47ee6f2f
JusesCheng <c6570002@gmail.com>
2020-05-08 18:53:04 +0800

    完成用套件的FAB，但icon不能客製

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <v-app class="grey lighten-4">
     <NavBar></NavBar>
+
+    <fab
+      position="bottom-right"
+      bg-color="#3DAD75"
+      main-icon="chat"
+      :actions="fabActions"
+      @Line="Line"
+      @WeChat="WeChat"
+      fixed-tooltip
+    ></fab>
     <!-- 裡面一定要用到不然會報錯(step.3) -->
 
     <v-content>
@@ -11,15 +21,39 @@
 
 <script>
 import NavBar from "@/components/Navbar";
+import fab from "vue-fab";
+
 //有新的Components這邊要先import(step.1)
 export default {
   name: "App",
 
   components: {
-    NavBar
+    NavBar,
+    fab
     //此處要註冊(step.2)
   },
 
-  data: () => ({})
+  data: () => ({
+    fabActions: [
+      {
+        name: "Line",
+        icon: "call_made",
+        tooltip: "Line"
+      },
+      {
+        name: "WeChat",
+        icon: "call_made",
+        tooltip: "WeChat"
+      }
+    ]
+  }),
+  methods: {
+    Line() {
+      console.log("Line");
+    },
+    WeChat() {
+      console.log("WeChat");
+    }
+  }
 };
 </script>
